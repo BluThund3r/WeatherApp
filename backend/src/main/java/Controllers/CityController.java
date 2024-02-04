c0bc39b0bf8e31f8281fb7d209fcf96ce035b9dd
Radu George-Marian <bluthundr3@gmail.com>
2024-02-04 14:28:04 +0200

    Added repositories, services, controllers (empty)

---
+++ backend/src/main/java/Controllers/CityController.java
@@ -0,0 +1,10 @@
+package Controllers;
+
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cities")
+public class CityController {
+}
--- backend/src/main/java/WeatherApp/backend/BackendApplication.java
+++ backend/src/main/java/WeatherApp/backend/BackendApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 @EntityScan(basePackages = {"Persistence"})
 public class BackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
