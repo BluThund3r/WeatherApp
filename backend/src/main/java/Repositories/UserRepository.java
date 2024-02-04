8fd56f0d19a360d941a58047d62448604426c798
Radu George-Marian <bluthundr3@gmail.com>
2024-02-04 14:55:59 +0200

    jwt in backend

--- backend/src/main/java/Repositories/UserRepository.java
+++ backend/src/main/java/Repositories/UserRepository.java
@@ -2,6 +2,9 @@ package Repositories;
 
 import Persistence.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("select u from User u where u.username = ?1")
+    User findByUsername(String username);
 }
--- backend/src/main/java/Services/UserService.java
+++ backend/src/main/java/Services/UserService.java
@@ -1,7 +1,21 @@
 package Services;
 
+import Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean isUsernameAvailable(String username) {
+        return userRepository.findByUsername(username) == null;
+    }
+
+
 }
